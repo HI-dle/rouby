d00e32ffdc3e11b0b9db8c0cc316da1a7203f568
hyezuu <duudu0704@gmail.com>
2025-07-06 20:50:24 +0900

    [#62][be][user]fix: UserJpaRepository 수정

--- back/src/main/java/com/rouby/user/infrastructure/persistence/jpa/UserJpaRepository.java
+++ back/src/main/java/com/rouby/user/infrastructure/persistence/jpa/UserJpaRepository.java
@@ -1,9 +1,10 @@
 package com.rouby.user.infrastructure.persistence.jpa;
 
 import com.rouby.user.domain.entity.User;
+import com.rouby.user.domain.repository.UserRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserJpaRepository extends
-    JpaRepository<User, Long>, UserJpaRepositoryCustom {
+    JpaRepository<User, Long>, UserJpaRepositoryCustom, UserRepository {
 
 }
