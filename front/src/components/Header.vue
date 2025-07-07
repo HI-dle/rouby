f800925a97a799a83ccb2360ccd23dbbcb4059ac
letsgilit <wjdrlf5781@naver.com>
2025-07-08 01:36:02 +0900

    [#105][fe][common]feat:header 컴포넌트 추가

---
+++ front/src/components/Header.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import HeaderIcon from '@/assets/header_logo.svg'
+</script>
+
+<template>
+  <div class="header">
+    <div class="brand">
+      <figure>
+        <img :src="HeaderIcon" alt="헤더 아이콘" >
+      </figure>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  max-width: 375px;
+  display: flex;
+  padding: 16px;
+  background-color: white;
+}
+
+.brand img {
+  height: 20px;
+  object-fit: contain;
+}
+
+.brand figure {
+  margin: 0;
+}
+</style>
