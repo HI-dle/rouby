724411fa21d169392982fce9d605bd3611085279
letsgilit <wjdrlf5781@naver.com>
2025-07-08 01:35:10 +0900

    [#105][fe][common]feat:tailwind config 수정

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['index.html', 'src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bodyText: '#6667D0', // 본문 텍스트 컬러
+        placeholderColor: '#9F7AEA',
+        contentColor: '#5A67D8',
+      }
+    },
   },
   plugins: [],
 }
