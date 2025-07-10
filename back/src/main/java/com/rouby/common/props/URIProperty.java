cc379e6c6608a1e60eb517628d8c28fbcd378ea1
letsgilit <wjdrlf5781@naver.com>
2025-07-11 04:57:48 +0900

    [#70][be][user]feat: BaseUrl 환경변수 추가

---
+++ back/src/main/java/com/rouby/common/props/URIProperty.java
@@ -0,0 +1,15 @@
+package com.rouby.common.props;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@ConfigurationProperties(prefix = "app")
+public class URIProperty {
+
+  private final String baseUrl;
+
+  public URIProperty(String baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+}
