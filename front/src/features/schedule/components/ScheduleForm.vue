de98efbcb8a1ab925c04065ce29dec6697cfa5f7
HanaH <hanahww2@gmail.com>
2025-07-10 22:32:04 +0900

    [#12][fe][schedule]style: css 수정

--- front/src/features/schedule/components/ScheduleForm.vue
+++ front/src/features/schedule/components/ScheduleForm.vue
@@ -26,7 +26,7 @@ const emit = defineEmits(['submit', 'cancel', 'inputDatetime', 'autoResize'])
           placeholder="제목"
           class="w-full text-base font-semibold border-b px-2 pb-2 mt-4 border-gray-300 focus:outline-none focus:border-black placeholder-gray-400 bg-inherit"
         />
-        <p v-if="errors.title" class="text-sm text-error-color mt-1 mx-2">{{ errors.title }}</p>
+        <p v-if="errors.title" class="text-sm text-error-color mt-2 mx-2">{{ errors.title }}</p>
       </div>
 
       <!-- 메모 -->
@@ -38,42 +38,43 @@ const emit = defineEmits(['submit', 'cancel', 'inputDatetime', 'autoResize'])
         @input="(e) => emit('autoResize', e)"
       />
 
-      <div class="grid grid-cols-[2fr_4fr] gap-4 xxs:grid-cols-[4fr_4fr]">
-        <!-- 하루종일 -->
-        <div class="flex justify-normal items-center">
-          <div class="flex items-center gap-2 mx-2 h-9">
-            <CalendarClock class="w-4 h-4 text-content-color" />
+      <div>
+        <div class="grid grid-cols-[2fr_4fr] gap-4 xxs:grid-cols-[4fr_4fr]">
+          <!-- 하루종일 -->
+          <div class="flex justify-normal items-center">
+            <div class="flex items-center gap-2 mx-2 h-9">
+              <CalendarClock class="w-4 h-4 text-content-color" />
+            </div>
+            <label class="ml-2 text-base font-semibold text-content-color">하루종일</label>
           </div>
-          <label class="ml-2 text-base font-semibold text-content-color">하루종일</label>
-        </div>
-        <div class="flex items-center justify-end mx-2">
-          <ToggleSwitch v-model="form.allDay" />
-        </div>
+          <div class="flex items-center justify-end mx-2">
+            <ToggleSwitch v-model="form.allDay" />
+          </div>
 
-        <!-- 시작 -->
-        <div class="flex items-center mx-2">
-          <label class="ml-8 text-base font-semibold text-content-color">시작</label>
-        </div>
-        <input
-          :type="form.allDay ? 'date' : 'datetime-local'"
-          :value="form.allDay ? toDate(form.start) : form.start"
-          @input="(e) => emit('input-datetime', e, 'start')"
-          class="w-full xxs:w-40 xxxs:w-[123px] text-base xs:text-sm xxs:text-xs text-content-color border rounded-md px-3 py-2 shadow-sm focus:outline-none focus:border-transparent focus:shadow-[0_0_3px_2px_theme(colors.main-color/30%)] transition"
-        />
+          <!-- 시작 -->
+          <div class="flex items-center mx-2">
+            <label class="ml-8 text-base font-semibold text-content-color">시작</label>
+          </div>
+          <input
+            :type="form.allDay ? 'date' : 'datetime-local'"
+            :value="form.allDay ? toDate(form.start) : form.start"
+            @input="(e) => emit('input-datetime', e, 'start')"
+            class="w-full xxs:w-40 xxxs:w-[123px] text-base xs:text-sm xxs:text-xs text-content-color border rounded-md px-3 xxs:px-[5px] py-2 shadow-sm focus:outline-none focus:border-transparent focus:shadow-[0_0_3px_2px_theme(colors.main-color/30%)] transition"
+          />
 
-        <!-- 종료 -->
-        <div class="flex items-center mx-2">
-          <label class="ml-8 text-base font-semibold text-content-color">종료</label>
+          <!-- 종료 -->
+          <div class="flex items-center mx-2">
+            <label class="ml-8 text-base font-semibold text-content-color">종료</label>
+          </div>
+          <input
+            :type="form.allDay ? 'date' : 'datetime-local'"
+            :value="form.allDay ? toDate(form.end) : form.end"
+            @input="(e) => emit('inputDatetime', e, 'end')"
+            class="w-full xxs:w-40 xxxs:w-[123px] text-base xs:text-sm xxs:text-xs text-content-color border rounded-md px-3 xxs:px-[5px] py-2 shadow-sm focus:outline-none focus:border-transparent focus:shadow-[0_0_3px_2px_theme(colors.main-color/30%)] transition"
+          />
         </div>
-        <input
-          :type="form.allDay ? 'date' : 'datetime-local'"
-          :value="form.allDay ? toDate(form.end) : form.end"
-          @input="(e) => emit('inputDatetime', e, 'end')"
-          class="w-full xxs:w-40 xxxs:w-[123px] text-base xs:text-sm xxs:text-xs text-content-color border rounded-md px-3 py-2 shadow-sm focus:outline-none focus:border-transparent focus:shadow-[0_0_3px_2px_theme(colors.main-color/30%)] transition"
-        />
-
         <!-- 에러 메시지 -->
-        <p v-if="errors.period" class="text-sm text-error-color mt-1 mx-2">{{ errors.period }}</p>
+        <p v-if="errors.period" class="text-sm text-error-color mt-2 mx-2">{{ errors.period }}</p>
       </div>
       <hr />
 
@@ -110,19 +111,21 @@ const emit = defineEmits(['submit', 'cancel', 'inputDatetime', 'autoResize'])
       </div>
 
       <!-- 루틴 시작 -->
-      <div class="grid grid-cols-[2fr_4fr] gap-4 xxs:grid-cols-[4fr_4fr]">
-        <div class="flex justify-normal items-center">
-          <div class="mx-2 h-9 flex items-center justify-center">
-            <Calendar class="w-4 h-4 text-content-color" />
+      <div>
+        <div class="grid grid-cols-[2fr_4fr] gap-4 xxs:grid-cols-[4fr_4fr]">
+          <div class="flex justify-normal items-center">
+            <div class="mx-2 h-9 flex items-center justify-center">
+              <Calendar class="w-4 h-4 text-content-color" />
+            </div>
+            <label class="ml-2 text-base font-semibold text-content-color">루틴 시작</label>
           </div>
-          <label class="ml-2 text-base font-semibold text-content-color">루틴 시작</label>
+          <input
+            type="date"
+            v-model="form.routineStart"
+            class="w-full xxxs:w-[120px] text-base xxs:text-xs text-content-color border rounded-md px-3 py-2 shadow-sm focus:outline-none focus:border-transparent focus:shadow-[0_0_3px_2px_theme(colors.main-color/30%)] transition"
+          />
         </div>
-        <input
-          type="date"
-          v-model="form.routineStart"
-          class="w-full xxxs:w-[120px] text-base xxs:text-xs text-content-color border rounded-md px-3 py-2 shadow-sm focus:outline-none focus:border-transparent focus:shadow-[0_0_3px_2px_theme(colors.main-color/30%)] transition"
-        />
-        <p v-if="errors.routineStart" class="text-sm text-error-color mt-1 mx-2">
+        <p v-if="errors.routineStart" class="text-sm text-error-color mt-2 mx-2">
           {{ errors.routineStart }}
         </p>
       </div>
