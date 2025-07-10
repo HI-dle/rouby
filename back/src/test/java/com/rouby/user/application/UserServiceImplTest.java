c628bc1fe1f0fe5f24fd8ad2b9abf4aab3496603
letsgilit <wjdrlf5781@naver.com>
2025-07-11 04:58:13 +0900

    [#70][be][user]feat: 비밀번호 찾기 Test 코드 구현

---
+++ back/src/test/java/com/rouby/user/application/UserServiceImplTest.java
@@ -0,0 +1,89 @@
+package com.rouby.user.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.rouby.common.security.WithMockCustomUser;
+import com.rouby.common.support.ControllerTestSupport;
+import com.rouby.user.presentation.dto.requset.FindPasswordRequest;
+import com.rouby.user.presentation.dto.requset.ResetPasswordRequest;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+class UserServiceImplTest extends ControllerTestSupport {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @WithMockCustomUser
+  @DisplayName("비밀번호 찾기 API")
+  @Test
+  void findPassword() throws Exception {
+    //given
+    FindPasswordRequest request = FindPasswordRequest.builder().email("test@email.com").build();
+
+    doNothing().when(userFacade).findPassword(any());
+
+    //when
+    ResultActions resultActions = mockMvc.perform(patch("/api/v1/users/find-password")
+        .content(objectMapper.writeValueAsString(request))
+        .contentType(MediaType.APPLICATION_JSON));
+
+    // then
+    resultActions.andExpect(status().isOk())
+        .andDo(print())
+        .andDo(document("find-password-user-200",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            requestFields(
+                fieldWithPath("email").description("유저 이메일"))
+        ));
+  }
+
+  @WithMockCustomUser
+  @DisplayName("비밀번호 변경 토큰 사용 API")
+  @Test
+  void resetPasswordByToken() throws Exception {
+    //given
+    ResetPasswordRequest request = ResetPasswordRequest.builder()
+        .currentPassword("currentPassword")
+        .newPassword("newPassword")
+        .build();
+
+    String token = UUID.randomUUID().toString();
+
+    doNothing().when(userFacade).resetPasswordByToken(any(),any());
+
+    //when
+    ResultActions resultActions = mockMvc.perform(patch("/api/v1/users/reset-password-by-token?token=" + token)
+        .content(objectMapper.writeValueAsString(request))
+        .contentType(MediaType.APPLICATION_JSON));
+
+    // then
+    resultActions.andExpect(status().isOk())
+        .andDo(print())
+        .andDo(document("reset-password-by-token-user-200",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            requestFields(
+                fieldWithPath("currentPassword").description("현재 비밀번호"),
+                fieldWithPath("newPassword").description("새 비밀번호"))
+        ));
+  }
+}
