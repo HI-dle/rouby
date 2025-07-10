c18d98faf080b2ead91abf0ae513063372fd43b1
letsgilit <wjdrlf5781@naver.com>
2025-07-11 04:55:24 +0900

    [#70][be][user]feat: error 코드 추가

--- back/src/main/java/com/rouby/user/application/exception/UserErrorCode.java
+++ back/src/main/java/com/rouby/user/application/exception/UserErrorCode.java
@@ -6,9 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 /**
- * @Date : 2025. 07. 08.
- *
  * @author : hanjihoon
+ * @Date : 2025. 07. 08.
  */
 @Getter
 @RequiredArgsConstructor
@@ -16,7 +15,11 @@ public enum UserErrorCode implements ErrorCode {
   //수정은 했는데 맞는 방향인지는 잘 모르겠습니다ㅜ!!
   INVALID_USER("잘못된 사용자 입니다.", "INVALID_USER", HttpStatus.UNAUTHORIZED),
   INVALID_USER_PASSWORD("잘못된 사용자 입니다", "INVALID_USER", HttpStatus.UNAUTHORIZED),
-  ;
+  EMAIL_NOT_FOUND("이메일을 찾을 수 없습니다.", "NOT_FOUND_EMAIL", HttpStatus.NOT_FOUND),
+  INVALID_PASSWORD("비밀번호가 일치하지 않습니다.", "INVALID_PASSWORD", HttpStatus.UNAUTHORIZED),
+  USER_NOT_FOUND("존재하지 않는 유저입니다.", "USER_NOT_FOUND", HttpStatus.NOT_FOUND),
+  PASSWORD_TOKEN_EXPIRED("비밀번호 재설정 토큰이 만료되었습니다.", "PASSWORD_TOKEN_EXPIRED",
+      HttpStatus.UNAUTHORIZED);
 
   private final String message;
   private final String code;
