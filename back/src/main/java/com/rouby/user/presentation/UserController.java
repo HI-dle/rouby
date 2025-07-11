6f104a8d1a6854f61c34e2d7aa5d99602368836a
letsgilit <wjdrlf5781@naver.com>
2025-07-11 17:07:18 +0900

    [#70][be][user]fix: api 수정 및 uri 변경

--- back/src/main/java/com/rouby/user/presentation/UserController.java
+++ back/src/main/java/com/rouby/user/presentation/UserController.java
@@ -1,13 +1,12 @@
 package com.rouby.user.presentation;
 
-import com.rouby.auth.dto.UserDetailsImpl;
 import com.rouby.user.application.UserFacade;
 import com.rouby.user.presentation.dto.requset.FindPasswordRequest;
 import com.rouby.user.presentation.dto.requset.ResetPasswordRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,18 +19,18 @@ public class UserController {
 
   private final UserFacade userFacade;
 
-  @PatchMapping("/reset-password-by-token")
+  @PatchMapping("/password/reset/token")
   public ResponseEntity<Void> resetPasswordByToken(
-      @RequestParam("token") String token, @RequestBody ResetPasswordRequest request) {
+      @RequestBody ResetPasswordRequest request) {
 
-    userFacade.resetPasswordByToken(token, request.toCommand());
+    userFacade.resetPasswordByToken(request.toCommand());
 
     return ResponseEntity.ok().build();
   }
 
-  @PatchMapping("/find-password")
+  @PostMapping("/password/find")
   public ResponseEntity<Void> findPassword(
-      @AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody FindPasswordRequest request) {
+      @RequestBody FindPasswordRequest request) {
 
     userFacade.findPassword(request.toCommand());
 
