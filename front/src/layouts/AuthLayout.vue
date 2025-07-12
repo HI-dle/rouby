dfdd15876971d7453188adb565e7281fa1de7295
hyezuu <duudu0704@gmail.com>
2025-07-13 00:24:25 +0900

    [#4][fe][user]fix: 인증 레이아웃 수정

--- front/src/layouts/AuthLayout.vue
+++ front/src/layouts/AuthLayout.vue
@@ -5,7 +5,7 @@ import Header from '@/components/Header.vue'
 <template>
   <div class="wrapper flex flex-col h-[calc(var(--vh,1vh)_*100)] overflow-hidden">
     <Header class="shrink-0"/>
-    <main class="flex flex-col lg:max-w-screen-md w-full mx-auto min-h-0">
+    <main class="flex flex-col max-w-md w-full mx-auto min-h-0">
       <router-view />
     </main>
   </div>
