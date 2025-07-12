ba12e3993ae035f9d7c17915b30ba6e8b2efd0b2
HanaH <hanahww2@gmail.com>
2025-07-13 05:46:34 +0900

    [#105][fe][common]refactor: 클래스 코드 보완

--- front/src/components/common/BaseModal.vue
+++ front/src/components/common/BaseModal.vue
@@ -1,10 +1,15 @@
 <script setup>
+import { cn } from '@/lib/utils'
 import BaseButton from './BaseButton.vue'
 
 const props = defineProps({
   modelValue: { type: Boolean, required: true },
   title: { type: String, default: '' },
   message: { type: String, default: '알림 메세지' },
+  class: {
+    type: String,
+    default: '',
+  },
   buttonClass: {
     type: String,
     default: '',
@@ -21,12 +26,14 @@ const close = () => {
     class="fixed inset-0 z-[9999] flex items-center justify-center bg-black bg-opacity-50"
     @click.self="close"
   >
-    <div class="bg-white rounded-xl shadow-lg w-full max-w-sm p-6 text-center">
+    <div
+      :class="[cn('bg-white rounded-xl shadow-lg w-full max-w-sm p-6 text-center', props.class)]"
+    >
       <h2 class="text-lg font-semibold text-main-color mb-4">{{ title }}</h2>
       <p class="text-sm text-gray-700 mb-6">{{ message }}</p>
       <BaseButton
         @click="$emit('update:modelValue', false)"
-        :class="['text-sm w-1/2', props.buttonClass]"
+        :class="['text-sm w-2/5 h-10', props.buttonClass]"
       >
         확인
       </BaseButton>
