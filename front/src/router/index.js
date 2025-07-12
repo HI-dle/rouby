3a3754afe3415f373cfedaa1a90f094bd323f7e2
hyezuu <duudu0704@gmail.com>
2025-07-12 22:14:21 +0900

    [#4][fe][user]feat: 라우팅 설정

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -60,6 +60,18 @@ const router = createRouter({
         // },
       ],
     },
+    {
+      path: '/auth',
+      component: () => import('@/layouts/AuthLayout.vue'),
+      children: [
+        {
+          path: 'signup',
+          name: 'signup',
+          component: () => import('@/features/auth/views/SignupView.vue'),
+        },
+      ]
+    },
+
   ],
 })
 
