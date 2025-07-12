c4da414aa0aea9e32ddb015c13297cda20ce052f
HanaH <hanahww2@gmail.com>
2025-07-12 17:45:00 +0900

    [#105][fe][common]feat: BaseButton 추가 및 그외 수정

---
+++ front/src/components/common/BaseButton.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'button', // 기본은 'button', 폼 제출용일 경우 'submit'을 명시적으로 전달
+  },
+  class: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<template>
+  <button
+    :type="type"
+    @click="$emit('click')"
+    :class="[
+      'w-full py-3 rounded-2xl text-white text-base font-semibold bg-gradient-to-r from-button-from to-button-to hover:from-[#546ac9] hover:to-[#6a4093] transition',
+      props.class,
+    ]"
+  >
+    <slot>로그인</slot>
+  </button>
+</template>
