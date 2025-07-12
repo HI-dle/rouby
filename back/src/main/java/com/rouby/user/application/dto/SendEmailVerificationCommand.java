87f3e437e6c735cb0e6aff42d043a8dc407e6eb9
cchoijjinyoung <cgy7127@gmail.com>
2025-07-12 19:48:41 +0900

    [#02][be][user]style: 알림 로그 패키지 변경

--- back/src/main/java/com/rouby/user/application/dto/SendEmailVerificationCommand.java
+++ back/src/main/java/com/rouby/user/application/dto/SendEmailVerificationCommand.java
@@ -1,9 +1,12 @@
 package com.rouby.user.application.dto;
 
+import com.rouby.notification.email.application.dto.SendVerificationEmailCommand;
+
 public record SendEmailVerificationCommand(
     String email
 ) {
-  public SendEmailCommand toSendEmailCommand(String to, String subject, String text) {
-    return new SendEmailCommand(to, subject, text);
+
+  public SendVerificationEmailCommand toSendEmailCommand(String to, String subject, String code) {
+    return SendVerificationEmailCommand.of(to, subject, code);
   }
 }
