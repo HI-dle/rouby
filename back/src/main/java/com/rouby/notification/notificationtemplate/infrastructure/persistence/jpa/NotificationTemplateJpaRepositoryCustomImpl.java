93eb41cafe29e94cc8fa6bb348a5251b8beb1340
cchoijjinyoung <cgy7127@gmail.com>
2025-07-12 19:43:34 +0900

    [#02][be][user]refactor: 이메일 로그 저장 시 이벤트 로직 제외

--- back/src/main/java/com/rouby/notification/notificationtemplate/infrastructure/persistence/jpa/NotificationTemplateJpaRepositoryCustomImpl.java
+++ back/src/main/java/com/rouby/notification/notificationtemplate/infrastructure/persistence/jpa/NotificationTemplateJpaRepositoryCustomImpl.java
@@ -1,4 +1,4 @@
-package com.rouby.notificationtemplate.infrastructure.persistence.jpa;
+package com.rouby.notification.notificationtemplate.infrastructure.persistence.jpa;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
