5eb9ae6a1e4ace064851eb06be4158eee750e428
letsgilit <wjdrlf5781@naver.com>
2025-07-15 12:54:48 +0900

    [#98][fe][user]fix: 헤더에 Bearer 추가

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -14,7 +14,7 @@ instance.interceptors.request.use(
   (config) => {
     const token = getStoredToken()
     if (token) {
-      config.headers.Authorization = `${token}`
+      config.headers.Authorization = "Bearer " + `${token}`
     }
     return config
   },
