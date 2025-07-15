9d650bc877ed5e8e8a8db17846b0469f41df2fee
hanjihoon03 <gkswlgns7653@gmail.com>
2025-07-15 20:27:15 +0900

    [#105][fe][common]feat: 메인 컬러 적용 및 svg추가

--- front/src/components/common/KeywordTag.vue
+++ front/src/components/common/KeywordTag.vue
@@ -2,13 +2,13 @@
 <template>
   <div
     v-if="isValid"
-    class="relative inline-flex items-center bg-indigo-100 text-indigo-600 text-sm font-bold px-4 py-2 rounded-full"
+    class="relative inline-flex items-center bg-[#677BE61A] text-main-color text-sm font-bold px-4 py-2 rounded-full"
   >
     <span>#{{ label }}</span>
 
     <button
       @click="emit('remove')"
-      class="absolute -top-1 -right-1 w-4 h-4 rounded-full bg-indigo-500 flex items-center justify-center text-white hover:bg-indigo-600 transition"
+      class="absolute -top-1 -right-1 w-4 h-4 rounded-full bg-main-color flex items-center justify-center text-white hover:bg-indigo-600 transition"
     >
       <X class="w-3 h-3" />
     </button>
