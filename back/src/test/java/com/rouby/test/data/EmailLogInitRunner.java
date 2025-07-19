915f234f8653a6583d68152185ee17fd173bc3f3
cchoijjinyoung <cgy7127@gmail.com>
2025-07-19 19:35:23 +0900

    [#173][be][notification]refactor: 이메일 타입 별 전송 수 조회 쿼리에 index 추가

--- back/src/test/java/com/rouby/test/data/EmailLogInitRunner.java
+++ back/src/test/java/com/rouby/test/data/EmailLogInitRunner.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-//@Disabled
+@Disabled
 public class EmailLogInitRunner {
 
   @Autowired EmailLogJpaRepository emailLogRepository;
