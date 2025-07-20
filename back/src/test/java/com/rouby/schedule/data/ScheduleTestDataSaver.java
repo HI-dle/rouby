3021734ca8a4e85c7a3b9bc3a307588598eb37b6
HanaH <hanahww2@gmail.com>
2025-07-20 14:56:38 +0900

    [#92][be][schedule]fix: 타이포 수정

--- back/src/test/java/com/rouby/schedule/data/ScheduleTestDataSaver.java
+++ back/src/test/java/com/rouby/schedule/data/ScheduleTestDataSaver.java
@@ -12,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class ScheduleTestDataSaver {
 
-  @Autowired\]
+  @Autowired
   private ScheduleRepository schedulesRepository;
 
   @Test
