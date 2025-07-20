d49a50dabd64943b7335e6b79767ae72612b7418
HanaH <hanahww2@gmail.com>
2025-07-20 15:00:32 +0900

    [#92][be][schedule]fix: disabled 활성화

--- back/src/test/java/com/rouby/schedule/data/ScheduleTestDataSaver.java
+++ back/src/test/java/com/rouby/schedule/data/ScheduleTestDataSaver.java
@@ -3,12 +3,13 @@ package com.rouby.schedule.data;
 import com.rouby.schedule.domain.entity.Schedule;
 import com.rouby.schedule.domain.repository.ScheduleRepository;
 import java.util.List;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@Disabled("필요한 경우 주석을 제거하고 사용해주세요.")
+@Disabled("필요한 경우 주석을 제거하고 사용해주세요.")
 @SpringBootTest
 public class ScheduleTestDataSaver {
 
