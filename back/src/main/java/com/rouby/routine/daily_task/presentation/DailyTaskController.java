d08aaa1df041e8661bf1b97ddda0b7113ec9063a
hyezuu <duudu0704@gmail.com>
2025-07-23 01:41:01 +0900

    [#76][be][routine] feat: daily-task 생성, 수정 api 구현

--- back/src/main/java/com/rouby/routine/daily_task/presentation/DailyTaskController.java
+++ back/src/main/java/com/rouby/routine/daily_task/presentation/DailyTaskController.java
@@ -1,7 +1,15 @@
 package com.rouby.routine.daily_task.presentation;
 
 import com.rouby.routine.daily_task.application.facade.DailyTaskFacade;
+import com.rouby.routine.daily_task.presentation.dto.ProgressDailyTaskRequest;
+import com.rouby.user.infrastructure.security.dto.SecurityUser;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +20,13 @@ public class DailyTaskController {
 
   private final DailyTaskFacade dailyTaskFacade;
 
+  @PostMapping("/progress")
+  @PreAuthorize("hasAnyRole('USER')")
+  public ResponseEntity<Void> progressDailyTask(
+      @AuthenticationPrincipal SecurityUser securityUser,
+      @RequestBody @Valid ProgressDailyTaskRequest request) {
+
+    dailyTaskFacade.progressDailyTask(request.toCommand(securityUser.getId()));
+    return ResponseEntity.ok().build();
+  }
 }
