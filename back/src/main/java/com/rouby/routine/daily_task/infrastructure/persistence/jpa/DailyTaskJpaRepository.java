0decab7acd472c0a2a958bd97e1f6698e08b0766
hyezuu <duudu0704@gmail.com>
2025-07-23 01:56:50 +0900

    [#76][be][routine] refactor: DailyTaskJpaRepository 수정

--- back/src/main/java/com/rouby/routine/daily_task/infrastructure/persistence/jpa/DailyTaskJpaRepository.java
+++ back/src/main/java/com/rouby/routine/daily_task/infrastructure/persistence/jpa/DailyTaskJpaRepository.java
@@ -2,11 +2,8 @@ package com.rouby.routine.daily_task.infrastructure.persistence.jpa;
 
 import com.rouby.routine.daily_task.domain.DailyTask;
 import com.rouby.routine.daily_task.domain.repository.DailyTaskRepository;
-import java.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DailyTaskJpaRepository extends DailyTaskRepository,
     JpaRepository<DailyTask, Long>, DailyTaskJpaRepositoryCustom {
-
-  boolean existsByRoutineTaskIdAndTaskDate(Long routineTaskId, LocalDate taskDate);
 }
