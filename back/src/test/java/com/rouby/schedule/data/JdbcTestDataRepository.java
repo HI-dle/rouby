65adb660f372eaee622ed06886b60a23b2372f69
letsgilit <wjdrlf5781@naver.com>
2025-07-31 23:18:14 +0900

    [#120][be][schedule] fix: BulkInsert 테스트코드 수정

--- back/src/test/java/com/rouby/schedule/data/JdbcTestDataRepository.java
+++ back/src/test/java/com/rouby/schedule/data/JdbcTestDataRepository.java
@@ -18,60 +18,75 @@ public class JdbcTestDataRepository {
   private final ObjectMapper objectMapper;
 
   @Transactional
-  public void batchInsert(int batchSize, List<Schedule> list) {
+  public void batchInsert(int batchSize, List<Schedule> schedules) {
 
     var sql = """
         INSERT INTO schedule 
-        (user_id, parent_schedule_id
+        (id, user_id, parent_schedule_id
         , title, memo, routine_offset_days, start_at, end_at
         , alarm_offset_type, override_type, override_date, recurrence_rule
         , created_at, created_by, updated_at, updated_by)
         VALUES
-        (?, ?
+        (?, ?, ?
         , ?, ?, ?, ?, ?
         , ?, ?, ?, ?::jsonb
         , NOW(), ?, NOW(), ?)
         """;
-
     try {
-      jdbcTemplate.batchUpdate(
-          sql,
-          list,
-          batchSize,
-          (ps, arg) -> {
-            ps.setLong(1, arg.getUserId());
-            ps.setObject(2,
-                arg.getParentSchedule() == null ? null : arg.getParentSchedule().getId(),
-                Types.BIGINT);
-            ps.setString(3, arg.getTitle());
-            ps.setString(4, arg.getMemo());
-            ps.setInt(5, arg.getRoutineOffsetDays());
-            ps.setTimestamp(6, Timestamp.valueOf(arg.getPeriod().getStartAt()));
-            ps.setTimestamp(7, Timestamp.valueOf(arg.getPeriod().getEndAt()));
-            ps.setObject(8,
-                arg.getAlarmOffsetType() == null ? null : arg.getAlarmOffsetType().name(),
-                Types.VARCHAR);
-            ps.setObject(9,
-                arg.getOverrideInfo() == null ? null : arg.getOverrideInfo().getOverrideType().name(),
-                Types.VARCHAR);
-            ps.setObject(10,
-                arg.getOverrideInfo() == null ? null : Date.valueOf(
-                    arg.getOverrideInfo().getOverrideDate()),
-                Types.DATE);
-            ps.setObject(11,
-                arg.getRecurrenceRule() == null
-                    ? null
-                    : toJsonString(arg.getRecurrenceRule())
-                        .replaceAll("\"byDay\":", "\"by_day\":"),
-                Types.VARCHAR);
-            ps.setLong(12, arg.getUserId());
-            ps.setLong(13, arg.getUserId());
-          });
-
+        batchUpdateSchedules(sql, batchSize, schedules);
     } catch (Exception e) {
       e.printStackTrace();
       throw e;
     }
+
+  }
+
+  private void batchUpdateSchedules(String sql, int batchSize, List<Schedule> schedules) {
+    jdbcTemplate.batchUpdate(
+        sql,
+        schedules,
+        batchSize,
+        (ps, arg) -> {
+          ps.setLong(1, arg.getId());
+          ps.setLong(2, arg.getUserId());
+          ps.setObject(3,
+              arg.getParentSchedule() == null ? null : arg.getParentSchedule().getId(),
+              Types.BIGINT);
+          ps.setString(4, arg.getTitle());
+          ps.setString(5, arg.getMemo());
+          ps.setInt(6, arg.getRoutineOffsetDays());
+          ps.setTimestamp(7, Timestamp.valueOf(arg.getPeriod().getStartAt()));
+          ps.setTimestamp(8, Timestamp.valueOf(arg.getPeriod().getEndAt()));
+          ps.setObject(9,
+              arg.getAlarmOffsetType() == null ? null : arg.getAlarmOffsetType().name(),
+              Types.VARCHAR);
+          ps.setObject(10,
+              arg.getOverrideInfo() == null ? null : arg.getOverrideInfo().getOverrideType().name(),
+              Types.VARCHAR);
+          ps.setObject(11,
+              arg.getOverrideInfo() == null ? null : Date.valueOf(
+                  arg.getOverrideInfo().getOverrideDate()),
+              Types.DATE);
+          ps.setObject(12,
+              arg.getRecurrenceRule() == null
+                  ? null
+                  : toJsonString(arg.getRecurrenceRule())
+                      .replaceAll("\"byDay\":", "\"by_day\":"),
+              Types.VARCHAR);
+          ps.setLong(13, arg.getUserId());
+          ps.setLong(14, arg.getUserId());
+        });
+  }
+
+  public List<Long> fetchNextIds(String sequenceName, int count) {
+    String sql = "SELECT nextval('schedule_seq') FROM generate_series(1, ?)";
+    return jdbcTemplate.query(
+        sql,
+        ps -> {
+          ps.setInt(1, count);
+        },
+        (rs, rowNum) -> rs.getLong(1)
+    );
   }
 
   private String toJsonString(Object object) {
--- back/src/test/java/com/rouby/schedule/data/ScheduleTestDataSaver.java
+++ back/src/test/java/com/rouby/schedule/data/ScheduleTestDataSaver.java
@@ -3,15 +3,16 @@ package com.rouby.schedule.data;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rouby.schedule.domain.entity.Schedule;
 import com.rouby.schedule.domain.repository.ScheduleRepository;
+import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.testcontainers.shaded.org.apache.commons.lang3.tuple.Pair;
 
-@Disabled("필요한 경우 해당 어노테이션을 주석 처리하고 사용해주세요.")
+//@Disabled("필요한 경우 해당 어노테이션을 주석 처리하고 사용해주세요.")
 @SpringBootTest
 public class ScheduleTestDataSaver {
 
@@ -29,15 +30,22 @@ public class ScheduleTestDataSaver {
   void createSchedules() {
 
     int batchSize = 100;
-    int maxSize = 1_000_000; // * 3 정도의 데이터 생성됨
+    int maxSize = 1_000; // * 3 정도의 데이터 생성됨
 
     JdbcTestDataRepository repository = new JdbcTestDataRepository(jdbcTemplate, objectMapper);
     List<Schedule> schedules;
+    int totalSize = ScheduleTestDataFactory.getCountSchedules(maxSize);
+
+    List<Long> ids = repository.fetchNextIds("schedule_id_seq", totalSize);
+
+    int idIndex = 0;
 
     for (int i = 0; i < maxSize; i += batchSize) {
       try {
-        schedules = ScheduleTestDataFactory.generateTestSchedules(i, batchSize);
+        schedules = ScheduleTestDataFactory.generateTestSchedules(i, batchSize, ids, idIndex);
         repository.batchInsert(batchSize, schedules);
+        idIndex += ScheduleTestDataFactory.getCountSchedules(Math.min(batchSize, maxSize - i));
+
         System.out.printf("Inserted batch %d-%d%n", i, i + batchSize);
 
       } catch (Exception e) {
