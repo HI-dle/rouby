0853152bce9390e66818291fe3bb27ddd57ad5bb
hyezuu <duudu0704@gmail.com>
2025-08-25 16:20:21 +0900

    [#71][be][routine] refactor: schema.sql 수정

--- back/src/main/resources/db/schema.sql
+++ back/src/main/resources/db/schema.sql
@@ -9,13 +9,11 @@ CREATE INDEX idx_schedule_parent_not_deleted
     WHERE deleted_at IS NULL;
 
 -- ROUTINE_TASKS
-DROP INDEX CONCURRENTLY IF EXISTS idx_rt_child_parent_active;
-CREATE INDEX CONCURRENTLY idx_rt_child_parent_active
+CREATE INDEX IF NOT EXISTS idx_rt_child_parent_active
     ON routine_tasks(parent_routine_task_id)
     WHERE deleted_at IS NULL;
 
 -- DAILY_TASKS
-DROP INDEX CONCURRENTLY IF EXISTS idx_daily_tasks_date_task_cover;
-CREATE INDEX CONCURRENTLY idx_daily_tasks_task_rid_date_cover
+CREATE INDEX IF NOT EXISTS idx_daily_tasks_task_rid_date_cover
     ON daily_tasks(routine_task_id, task_date)
     INCLUDE (current_value);
