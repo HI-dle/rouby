8d96d34637292ff83fe3ce94923a91189d451982
letsgilit <wjdrlf5781@naver.com>
2025-09-05 17:57:49 +0900

    [#222][be][notification]fix: 코드리뷰 반영

--- back/src/main/java/com/rouby/notification/notificationtemplate/domain/entity/Message.java
+++ back/src/main/java/com/rouby/notification/notificationtemplate/domain/entity/Message.java
@@ -6,27 +6,44 @@ import java.io.Serializable;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import org.springframework.util.StringUtils;
 
 @Getter
 @Embeddable
 @EqualsAndHashCode
 public class Message implements Serializable {
 
-  @Column(length = 500, nullable = false)
+  @Column(length = 30, nullable = false)
   private String title;
 
-  @Column(length = 1000, nullable = false)
+  @Column(length = 100, nullable = false)
   private String body;
 
   public static Message of(String title, String body) {
-    if (!StringUtils.hasText(title) || !StringUtils.hasText(body)) {
-      throw new IllegalArgumentException("알림 템플릿 메시지는 비어 있을 수 없습니다.");
-    }
+    validateTitle(title);
+    validateBody(body);
+
     return Message.builder()
         .title(title)
         .body(body)
         .build();
+  }
+
+  private static void validateTitle(String title) {
+    if (title == null || title.trim().isEmpty()) {
+      throw new IllegalArgumentException("알림 제목(title)은 비어 있을 수 없습니다.");
+    }
+    if (title.length() > 30) {
+      throw new IllegalArgumentException("알림 제목은 500자를 초과할 수 없습니다.");
+    }
+  }
+
+  private static void validateBody(String body) {
+    if (body == null || body.trim().isEmpty()) {
+      throw new IllegalArgumentException("알림 본문(body)은 비어 있을 수 없습니다.");
+    }
+    if (body.length() > 100) {
+      throw new IllegalArgumentException("알림 본문은 1000자를 초과할 수 없습니다.");
+    }
   }
 
   @Builder
